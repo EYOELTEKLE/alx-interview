3a7bcc7f9ba70496b4c280436339e62d42e517de
Eyoel Tekle <56158187+EYOELTEKLE@users.noreply.github.com>
2023-01-06 16:46:48 +0300

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,38 +10,17 @@ def canUnlockAll(boxes):
     based on keys that can be attained.
     Solution to the lockboxes problem
     """
-    seen = [False for i in range(len(boxes))]
-    def getGraph(graph={}):
-        """
-        adjacencyList graph
-        """
-        for i in range(len(boxes)):
-            for j in range(len(boxes[i])):
-                if i not in graph:
-                    graph[i] = []
-                graph[i].append(boxes[i][j])
-        return graph
-    graph = getGraph()
+    if (type(boxes)) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
 
-    def dfs(src):
-        """
-        DFS function
-        """
-        stack = [src]
-        while len(stack) > 0:
-            current = stack.pop()
-            if current in visited:
-                continue
-            visited.add(current)
-            if int(current) != int(src):
-                seen[int(current)] = True
-            if current in graph:
-                for item in graph[current]:
-                    stack.append(item)
-    visited = set()
-    seen[0] = True
-    for item in graph:
-        dfs(item)
-    for val in seen:
-        if val is False:
-            return False
+    for k in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for idx in range(len(boxes)):
+            boxes_checked = k in boxes[idx] and k != idx
+            if boxes_checked:
+                break
+        if boxes_checked is False:
+            return boxes_checked
+    return True
